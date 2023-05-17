47cc9a3ea3585d15df3747e798886d121c7c5ba4
Simeet Nayan <simeetnayan.8100@gmail.com>
2023-05-17 13:19:15 +0530

    Added functionalities

--- utils/perform_search.py
+++ utils/perform_search.py
@@ -1,18 +1,12 @@
-pinecone.init(api_key=PINECONE_KEY, environment="asia-southeast1-gcp-free")
-pinecone_index = pinecone.Index("leo-search")
-from utils.key import PINECONE_KEY
-from utils import hf
 
+from utils import hf
+from utils import pinecone_index
 def sem_search(query):
+	print(query)
 	qemb=hf.embed_query(query)
-	xc = pinecone_index.query(qemb, top_k=5, include_metadata=True)
+	print('d2')
+	xc = pinecone_index.query(qemb, top_k=3, include_metadata=True)
 	results=[]
 	for result in xc['matches']:
 		results.append({'title': result['metadata']['question'], 'url':'google.com'})
 	return results
-	# print(PINECONE_KEY)
-	# results = [
-    #     {'title': 'Result 1', 'url': 'http://example.com/result1'},
-    #     {'title': 'Result 2', 'url': 'http://example.com/result2'},
-    #     {'title': 'Result 3', 'url': 'http://example.com/result3'}]
-	# return results
