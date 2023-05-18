aeafddeeab2de72765fe9769b552a57561a700fd
Simeet Nayan <simeetnayan.8100@gmail.com>
2023-05-18 09:27:09 +0530

    Changed perform_search

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def search():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '0.0.0.0', debug =True )
--- utils/perform_search.py
+++ utils/perform_search.py
@@ -4,9 +4,12 @@ from utils import pinecone_index
 def sem_search(query):
 	print(query)
 	qemb=hf.embed_query(query)
-	print('d2')
-	xc = pinecone_index.query(qemb, top_k=3, include_metadata=True)
+	#print('d2')
+	xc = pinecone_index.query(qemb, top_k=7, include_metadata=True)
 	results=[]
+	cnt=1
 	for result in xc['matches']:
-		results.append({'title': result['metadata']['question'], 'url':'google.com'})
+		rank = 'Rank: ' + str(cnt)
+		results.append({'title': result['metadata']['question'], 'rank': rank})
+		cnt=cnt+1
 	return results
